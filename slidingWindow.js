11c072f0008c268430e30ca06826b3fb0315db51
aaabdelgany <42511430+aaabdelgany@users.noreply.github.com>
2021-08-17 18:06:13 -0400

    main

--- slidingWindow.js
+++ slidingWindow.js
@@ -15,11 +15,11 @@ const maxSubarraySum = (arr1,max)=>{
     }
     return mainMax
 }
-console.log(maxSubarraySum([100,200,300,400],2))
-console.log(maxSubarraySum([1,4,2,10,23,3,1,0,20],4))
-console.log(maxSubarraySum([-3,4,0,-2,6,-1],2))
-console.log(maxSubarraySum([3,-2,7,-4,1,-1,4,-2,1],2))
-console.log(maxSubarraySum([2,3],3))
+// console.log(maxSubarraySum([100,200,300,400],2))
+// console.log(maxSubarraySum([1,4,2,10,23,3,1,0,20],4))
+// console.log(maxSubarraySum([-3,4,0,-2,6,-1],2))
+// console.log(maxSubarraySum([3,-2,7,-4,1,-1,4,-2,1],2))
+// console.log(maxSubarraySum([2,3],3))
 
 
 //minSubArrayLen
@@ -48,6 +48,33 @@ const minSubArrayLen = (arr1,int)=>{
     return mainSum===Infinity?0 : mainSum
 }
 
-console.log(minSubArrayLen([2, 3, 1, 2, 4, 3], 7)); // 2 -> because [4, 3] is the smallest subarray
-console.log(minSubArrayLen([2, 1, 6, 5, 4], 9)); // 2 -> because [5, 4] is the smallest subarray
-console.log(minSubArrayLen([3, 1, 62, 19], 52)); // 1 -> because [62] is greater than 52
+// console.log(minSubArrayLen([2, 3, 1, 2, 4, 3], 7)); // 2 -> because [4, 3] is the smallest subarray
+// console.log(minSubArrayLen([2, 1, 6, 5, 4], 9)); // 2 -> because [5, 4] is the smallest subarray
+// console.log(minSubArrayLen([3, 1, 62, 19], 52)); // 1 -> because [62] is greater than 52
+
+//Write a function called findLongestSubstring, which accepts a string and returns the length of the 
+//longest substring with all distinct characters.
+
+const findLongestSubstring = (str1)=>{
+    let tempLong = 0
+    let mainLong = 0
+    let start=0
+    const counter={}
+    for(let i =0;i<str1.length;i++){
+        let char=str1[i]
+        if(counter[char]){
+            mainLong=Math.max(tempLong,mainLong)
+        }
+        else{
+            counter[str1[i]]=1
+            tempLong+=1
+        }
+    }
+    console.log(mainLong)
+    return(mainLong)
+}
+
+findLongestSubstring(''); // 0
+findLongestSubstring('rithmschool'); // 7
+findLongestSubstring('thecatinthehat'); // 7
+findLongestSubstring('bbbbbb'); // 1
