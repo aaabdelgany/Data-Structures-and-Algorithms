118491c21e8ce6e7723a56571a5cfcc5575dd907
aaabdelgany <42511430+aaabdelgany@users.noreply.github.com>
2021-08-30 16:26:57 -0400

    Update sort.js

--- sort.js
+++ sort.js
@@ -17,7 +17,9 @@ const bubbleSort = (arr) => {
 // console.log(bubbleSort([1, 2, 3, 5, 7, 4]));
 // console.log(bubbleSort([48, 78, 1, 2, 3, 5, 7, 4, 22, 15, 17, 98, 40, 16]));
 
-//slightly better, less comparisons since we're starting from the end
+//slightly better, less comparisons since we're starting from the end. Also adds a break to stop comparisons once
+//we are all set. this would mean the best case time is linear(only if the data is almost fully sorted). IF it isnt nearly sorted
+//its quadratic time
 const betterBubble = (arr) => {
   for (let i = arr.length; i > 0; i--) {
     let noSwaps = true;
