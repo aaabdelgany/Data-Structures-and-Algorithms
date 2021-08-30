3b7cfafe670dd718d6268b15df65333931b366a7
aaabdelgany <42511430+aaabdelgany@users.noreply.github.com>
2021-08-30 16:25:33 -0400

    Update sort.js

--- sort.js
+++ sort.js
@@ -15,4 +15,25 @@ const bubbleSort = (arr) => {
 };
 
 // console.log(bubbleSort([1, 2, 3, 5, 7, 4]));
-console.log(bubbleSort([48, 78, 1, 2, 3, 5, 7, 4, 22, 15, 17, 98, 40, 16]));
+// console.log(bubbleSort([48, 78, 1, 2, 3, 5, 7, 4, 22, 15, 17, 98, 40, 16]));
+
+//slightly better, less comparisons since we're starting from the end
+const betterBubble = (arr) => {
+  for (let i = arr.length; i > 0; i--) {
+    let noSwaps = true;
+    for (let j = 0; j < i - 1; j++) {
+      if (arr[j] > arr[j + 1]) {
+        const tempJ = arr[j];
+        arr[j] = arr[j + 1];
+        arr[j + 1] = tempJ;
+        noSwaps = false;
+      }
+    }
+    console.log('one pass done');
+    if (noSwaps) {
+      break;
+    }
+  }
+  return arr;
+};
+console.log(betterBubble([48, 78, 1, 2, 3, 5, 7, 4, 22, 15, 17, 98, 40, 16]));
