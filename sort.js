8da0fe583966ecc36a3983cb76d1bf6d77bcae48
aaabdelgany <42511430+aaabdelgany@users.noreply.github.com>
2021-08-31 18:01:29 -0400

    Update sort.js

--- sort.js
+++ sort.js
@@ -31,7 +31,6 @@ const betterBubble = (arr) => {
         noSwaps = false;
       }
     }
-    console.log('one pass done');
     if (noSwaps) {
       break;
     }
@@ -39,3 +38,24 @@ const betterBubble = (arr) => {
   return arr;
 };
 console.log(betterBubble([48, 78, 1, 2, 3, 5, 7, 4, 22, 15, 17, 98, 40, 16]));
+
+//this is quadratic time, its better than bubblesort in only one aspect - space complexity.
+//theres only ever 1 swap so it doesnt take much space.
+const selectionSort = (arr) => {
+  let index;
+  for (let i = 0; i < arr.length; i++) {
+    let min = arr[i];
+    for (let j = i + 1; j < arr.length; j++) {
+      if (min > arr[j]) {
+        min = arr[j];
+        index = j;
+      }
+    }
+    if (min != arr[i]) {
+      arr[index] = arr[i];
+      arr[i] = min;
+    }
+  }
+  return arr;
+};
+console.log(selectionSort([5, 4, 3, 2, 1]));
